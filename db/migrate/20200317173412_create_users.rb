472dbc643b14dc394c0ffc97535db9bdce60ddd1
Daniel Ha <dha@interweb-solutions.com>
2020-03-17 14:04:25 -0700

    Create User model

--- db/migrate/20200317173412_create_users.rb
+++ db/migrate/20200317173412_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_hash
-      t.string :first_name
-      t.string :last_name
+      t.string :email, null: false
+      t.string :password_hash, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.index :email, unique: true
 
       t.timestamps
     end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,30 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'user with a valid email should be valid' do
+    user = User.new(email: 'test@test.org', password_hash: 'test', first_name: 'Test', last_name: 'Test')
+    assert user.valid?
+  end
+
+  test 'user with a invalid email should be invalid' do
+    user = User.new(email: 'test', password_hash: 'test', first_name: 'Test', last_name: 'Test')
+    assert_not user.valid?
+  end
+
+  test 'user with taken email should be invalid' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_hash: 'test', first_name: 'Test', last_name: 'Test')
+    assert_not user.valid?
+  end
+
+  test 'user with missing first name should be invalid' do
+    user = User.new(email: 'test@test.org', password_hash: 'test', last_name: 'Test')
+    assert_not user.valid?
+  end
+
+  test 'user with missing last name should be invalid' do
+    user = User.new(email: 'test@test.org', password_hash: 'test')
+    assert_not user.valid?
+  end
+
 end
